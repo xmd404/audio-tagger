6caf347109d09d216c93707fa77302b719460759
Xavier Duncan <xduncan@pm.me>
2018-09-09 18:14:21 -0400

    update app :)

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,36 @@
-function getCurTime() {
-    var audio = document.getElementById("myAudio");
-    var text = '<li>text</li>';
-
-    var name = document.getElementById("myName").value;
-    var comment = document.getElementById("myComment").value;
-    var timeStamp = audio.currentTime;
-
-    var postedComment = name + ":" + " " + comment + " " + "@" + " " + timeStamp;
-
-    var addPostedComment = 
-    document.getElementById("comments").innerHTML = postedComment;
-
-    
-
-}
+// get name & comment values
+let getNameComment = () => {
+    let name = document.getElementsByClassName('name')[0].value;
+    let comment = document.getElementsByClassName('comment')[0].value;
+    return [name, comment];
+};
+// get audio time stamp
+let getTimeStamp = () => {
+    let audio = document.getElementsByClassName("audio")[0];
+    let seconds = parseInt(audio.currentTime % 60);
+    let minutes = parseInt((audio.currentTime / 60) % 60);
+    let audioTimeStamp = minutes + ':' + seconds;
+    return audioTimeStamp;
+};
+// bind data to audio time stamp
+let bindData = () => {
+    [name, comment] = getNameComment();
+    audioTimeStamp = getTimeStamp();
+    console.log(name, comment, audioTimeStamp);
+    // create <p> element
+    // get element by listItemData class name
+    // init listItemData text
+    let listItemData = document.createElement('p');
+    listItemData.classList.add('listItemData');
+    listItemData.innerText = `${name}: ${comment} @ ${audioTimeStamp}`;
+    // create <li> element
+    // get element by listItem class name
+    // append listItemData to listItem
+    let listItem = document.createElement('li');
+    listItem.classList.add('listItem');
+    listItem.appendChild(listItemData);
+    // get element by comments id name
+    // append listItem to commentList
+    let commentList = document.getElementById('comments');
+    commentList.appendChild(listItem);
+};
