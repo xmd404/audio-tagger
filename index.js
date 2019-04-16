f8743d81dd9f081f3c95b14aa1bd1c0230e8868b
Xavier Duncan <xduncan@pm.me>
2019-04-15 20:51:17 -0400

    remove uneeded comments, format

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const getNameComment = (name, comment) => {
     name = document.getElementsByClassName('name')[0].value;
     comment = document.getElementsByClassName('comment')[0].value;
     return [name, comment];
-};
+}
 // get audio time stamp
 const getTimeStamp = (audio, seconds, minutes, audioTimeStamp) => {
     audio = document.getElementsByClassName("audio")[0];
@@ -15,7 +15,7 @@ const getTimeStamp = (audio, seconds, minutes, audioTimeStamp) => {
     minutes = parseInt((audio.currentTime / 60) % 60);
     audioTimeStamp = minutes + ':' + seconds;
     return audioTimeStamp;
-};
+}
 // bind data to audio time stamp
 const bindData = () => {
     [name, comment] = getNameComment();
@@ -28,25 +28,17 @@ const bindData = () => {
         console.log(data);
         return data;
     }
-};
+}
 // render data to screen
 const renderData = (data) => {
     const { name, comment, audioTimeStamp } = data;
-    // create <p> element
-    // get element by listItemData class name
-    // init listItemData text
     let listItemData = document.createElement('p');
     listItemData.classList.add('listItemData');
     listItemData.innerText = `${name}: ${comment} @ ${audioTimeStamp}`;
-    // create <li> element
-    // get element by listItem class name
-    // append listItemData to listItem
     let listItem = document.createElement('li');
     listItem.classList.add('listItem');
     listItem.appendChild(listItemData);
-    // get element by comments id name
-    // append listItem to commentList
     let commentList = document.createElement('ul');
     commentList.classList.add('commentList');
     commentList.appendChild(listItem);
-};
+}
