27355a37be94e6a7ee15d4f8bab6e049dbb64745
Xavier Duncan <xduncan@pm.me>
2019-06-10 13:53:12 -0500

    refactor js

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
-// build homepage
-const HomePage = () => {
-  const root = document.getElementById('root');
-}
 // get name & comment values
-const getNameComment = () => {
-	let name = document.getElementsByClassName('name')[0].value;
-	let comment = document.getElementsByClassName('comment')[0].value;
+const getNameComment = (name, comment) => {
+	name = document.getElementsByClassName('name')[0].value;
+	comment = document.getElementsByClassName('comment')[0].value;
 	return [name, comment];
-};
+}
 // get audio time stamp
-const getTimeStamp = () => {
-	let audio = document.getElementsByClassName("audio")[0];
-	let seconds = parseInt(audio.currentTime % 60);
-	let minutes = parseInt((audio.currentTime / 60) % 60);
-	let audioTimeStamp = minutes + ':' + seconds;
+const getTimeStamp = (audio, seconds, minutes, audioTimeStamp) => {
+	audio = document.getElementsByClassName("audio")[0];
+	seconds = parseInt(audio.currentTime % 60);
+	minutes = parseInt((audio.currentTime / 60) % 60);
+	audioTimeStamp = minutes + ':' + seconds;
 	return audioTimeStamp;
-};
+}
 // bind data to audio time stamp
 const bindData = () => {
 	[name, comment] = getNameComment();
 	audioTimeStamp = getTimeStamp();
-	console.log(name, comment, audioTimeStamp);
-	// create <p> element
-	// get element by listItemData class name
-	// init listItemData text
 	let listItemData = document.createElement('p');
 	listItemData.classList.add('listItemData');
 	listItemData.innerText = `${name}: ${comment} @ ${audioTimeStamp}`;
-	// create <li> element
-	// get element by listItem class name
-	// append listItemData to listItem
 	let listItem = document.createElement('li');
 	listItem.classList.add('listItem');
 	listItem.appendChild(listItemData);
-	// get element by comments id name
-	// append listItem to commentList
 	let commentList = document.getElementById('comments');
 	commentList.appendChild(listItem);
 };
