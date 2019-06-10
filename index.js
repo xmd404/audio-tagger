7331f326903951e4328a84498a47a6d95d52a871
Xavier Duncan <xduncan@pm.me>
2019-06-10 13:31:16 -0500

    remove copy.html, revert back to older version

--- index.js
+++ index.js
@@ -1,102 +1,40 @@
-// capture name & comment
-const getName = () => {
+// build homepage
+const HomePage = () => {
+  const root = document.getElementById('root');
+}
+// get name & comment values
+const getNameComment = () => {
 	let name = document.getElementsByClassName('name')[0].value;
-	return name;
-};
-const getComment = () => {
 	let comment = document.getElementsByClassName('comment')[0].value;
-	return comment;
+	return [name, comment];
 };
-// capture audio timestamp
+// get audio time stamp
 const getTimeStamp = () => {
-	let audio = document.getElementsByClassName('audio')[0];
+	let audio = document.getElementsByClassName("audio")[0];
 	let seconds = parseInt(audio.currentTime % 60);
 	let minutes = parseInt((audio.currentTime / 60) % 60);
-	let audioTimeStamp = `${minutes} : ${seconds}`;
+	let audioTimeStamp = minutes + ':' + seconds;
 	return audioTimeStamp;
 };
-// bind name & comment to audio timestamp
-const bindData = (e) => {
-	e.preventDefault();
-	name = getName();
-	comment = getComment();
+// bind data to audio time stamp
+const bindData = () => {
+	[name, comment] = getNameComment();
 	audioTimeStamp = getTimeStamp();
-	const data = {
-		name,
-		comment,
-		audioTimeStamp
-	};
-	if (data.name == '' || data.comment == '') {
-		console.log('Error: form not complete!');
-		alert('must complete form!');
-	} else {
-		console.log('Success: ', {
-			data
-		});
-		renderData(data);
-		localStorage.setItem('tag', JSON.stringify(data));
-		form.reset();
-	}
-};
-// render data to screen
-const renderData = ({ name, comment, audioTimeStamp }) => {
+	console.log(name, comment, audioTimeStamp);
+	// create <p> element
+	// get element by listItemData class name
+	// init listItemData text
 	let listItemData = document.createElement('p');
 	listItemData.classList.add('listItemData');
 	listItemData.innerText = `${name}: ${comment} @ ${audioTimeStamp}`;
+	// create <li> element
+	// get element by listItem class name
+	// append listItemData to listItem
 	let listItem = document.createElement('li');
 	listItem.classList.add('listItem');
 	listItem.appendChild(listItemData);
-	let commentList = document.createElement('ul');
-	commentList.classList.add('commentList');
+	// get element by comments id name
+	// append listItem to commentList
+	let commentList = document.getElementById('comments');
 	commentList.appendChild(listItem);
-	root.appendChild(commentList);
 };
-// build homepage
-const HomePage = () => {
-	// title + description
-	const title = document.createElement('h1');
-	title.classList.add('title');
-	title.innerText = 'audio-tagger';
-	const description = document.createElement('p');
-	description.classList.add('description');
-	description.innerText =
-		'A custom audio feature built for the SoundTap platform. It allows users to post data that binds to time stamps. During playback, the data will appear during the time stamp it originally posted at.';
-	// audio player
-	const audioSource = document.createElement('source');
-	audioSource.setAttribute('src', './assets/coltrane.mp3');
-	audioSource.setAttribute('type', 'audio/mp3');
-	const audioPlayer = document.createElement('audio');
-	audioPlayer.classList.add('audio');
-	audioPlayer.setAttribute('controls', 'controls');
-	audioPlayer.innerText = 'Your browser does not support HTML5 audio.';
-	// comment form
-	audioPlayer.appendChild(audioSource);
-	const nameField = document.createElement('input');
-	nameField.classList.add('name');
-	nameField.setAttribute('type', 'text');
-	nameField.setAttribute('placeholder', 'Name');
-	nameField.setAttribute('required', 'required');
-	const commentField = document.createElement('input');
-	commentField.classList.add('comment');
-	commentField.setAttribute('type', 'text');
-	commentField.setAttribute('placeholder', 'Comment');
-	commentField.setAttribute('required', 'required');
-	const submitButton = document.createElement('input');
-	submitButton.setAttribute('type', 'submit');
-	submitButton.setAttribute('value', 'Submit');
-	submitButton.addEventListener('click', bindData);
-	const form = document.createElement('form');
-	form.id = 'form';
-	form.appendChild(nameField);
-	form.appendChild(commentField);
-	form.appendChild(submitButton);
-	// append page elements to root
-	const root = document.getElementById('root');
-	root.appendChild(title);
-	root.appendChild(description);
-	root.appendChild(attribution);
-	root.appendChild(audioPlayer);
-	root.appendChild(form);
-};
-
-HomePage();
