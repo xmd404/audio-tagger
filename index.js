788fb9cd0ee9b53197ab2a44fe4cc73668c5bfd7
Xavier Duncan <xduncan@pm.me>
2019-07-13 20:31:46 -0500

    add upload functionality

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// display uploaded sound file
+input.onchange = function(e){
+  var sound = document.getElementById('sound');
+  sound.src = URL.createObjectURL(this.files[0]);
+  // not really needed in this exact case, but since it is really important in other cases,
+  // don't forget to revoke the blobURI when you don't need it
+  sound.onend = function(e) {
+    URL.revokeObjectURL(this.src);
+  }
+}
 // get name & comment values
 const getNameComment = (name, comment) => {
 	name = document.getElementsByClassName('name')[0].value;
