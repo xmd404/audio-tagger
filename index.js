2c71659b4e9f5a78699cb904e204995bf5ad0932
Xavier Duncan <xduncan@pm.me>
2019-12-15 03:51:23 -0600

    update padding & margin

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // display uploaded sound file
 input.onchange = function(e){
-  var sound = document.getElementById('sound');
+  const sound = document.getElementById('sound');
   sound.src = URL.createObjectURL(this.files[0]);
   // not really needed in this exact case, but since it is really important in other cases,
   // don't forget to revoke the blobURI when you don't need it
   sound.onend = function(e) {
     URL.revokeObjectURL(this.src);
-  }
-}
+	}
+};
 // get name & comment values
 const getNameComment = (name, comment) => {
 	name = document.getElementsByClassName('name')[0].value;
 	comment = document.getElementsByClassName('comment')[0].value;
 	return [name, comment];
-}
+};
 // get audio time stamp
 const getTimeStamp = (audio, seconds, minutes, audioTimeStamp) => {
 	audio = document.getElementsByClassName("audio")[0];
@@ -21,7 +21,7 @@ const getTimeStamp = (audio, seconds, minutes, audioTimeStamp) => {
 	minutes = parseInt((audio.currentTime / 60) % 60);
 	audioTimeStamp = minutes + ':' + seconds;
 	return audioTimeStamp;
-}
+};
 // bind data to audio time stamp
 const bindData = () => {
 	[name, comment] = getNameComment();
